dcf8935dfa84858783b1bc84eba1bd56a2a829a2
Dell <gabiwoutersm@gmail.com>
2026-02-04 17:03:08 -0300

    Início - Commit inicial - lista de palavras possíveis

---
+++ main.py
@@ -0,0 +1,8 @@
+import random as rdm
+
+palavras = {
+    'animal' : ['raposa', 'elefante', 'pinguim', 'cachorro', 'gato'],
+    'construção' : ['prédio', 'casa', 'loja', 'fazenda'],
+    'linguagem de programação': ['python', 'java', 'javascript'],
+    'fruta' : ['banana', 'maça', 'abacate', 'uva', 'tomate', 'kiwi']
+}
