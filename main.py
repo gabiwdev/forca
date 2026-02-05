8ccddd884a5af9cf8465d716ef0cb8650d304bd9
Dell <gabiwoutersm@gmail.com>
2026-02-05 14:03:50 -0300

    Minor add - Adicionado comentários e descrição de funções - Removido linhas teste

--- main.py
+++ main.py
@@ -59,10 +59,10 @@ letras = set()
 
 print(f"Categoria: {categoria}")
 
-print(palavra)
-print(descobrir)
 
 def validar_letra(letra):
+    """Valida a letra que o usuário chuta no loop
+    Só será True quando for apenas uma letra e faça parte do alfabeto"""
     if len(letra) != 1:
         print('Insira apenas uma letra!')
         return False
@@ -72,16 +72,17 @@ def validar_letra(letra):
     return True
 
 def adicionar_letra(letter, word, incognita):
+    """Atualiza a palavra não-descoberta pra conter a letra na posição exata
+    que ela está na palavra "word" passada"""
     for index, let in enumerate(word):
         if letter == word[index]:
             incognita[index] = letter
     print(f'A letra {letter} estava na palavra!\n{"".join(incognita)}')
     return
 
-
+# Loop principal do jogo, não encerra enquanto o usuário não descobrir a palavra ou esgotar tentativas
 while not descoberta and tentativas > 0:
     letra = input('Insira uma letra: ').strip().lower()
-    print(letra)
     if validar_letra(letra):
         if letra in letras:
             print(f'Você já tentou a letra "{letra}".\nAs letras já tentadas foram {letras}')
@@ -96,6 +97,8 @@ while not descoberta and tentativas > 0:
             print(f'A letra {letra} não existe na palavra!\nTentativas restantes: {tentativas}')
     else:
         continue
+
+    ## Checa se o usuário descobriu a palavra
     if (''.join(descobrir)) == palavra:
         descoberta = True
         print('Parabéns, você acertou a palavra')
