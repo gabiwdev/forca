31b85381f7705b8a25d9429c48087b1939419863
Dell <gabiwoutersm@gmail.com>
2026-02-05 09:13:25 -0300

    Implementação - Seleção de categoria - Seleção da palavra

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 import random as rdm
 
 palavras = {
-    'animal' : ['raposa', 'elefante', 'pinguim', 'cachorro', 'gato'],
-    'construção' : ['prédio', 'casa', 'loja', 'fazenda'],
+    'animal': ['raposa', 'elefante', 'pinguim', 'cachorro', 'gato'],
+    'construção': ['prédio', 'casa', 'loja', 'fazenda'],
     'linguagem de programação': ['python', 'java', 'javascript'],
-    'fruta' : ['banana', 'maça', 'abacate', 'uva', 'tomate', 'kiwi']
+    'fruta': ['banana', 'maça', 'abacate', 'uva', 'tomate', 'kiwi']
 }
+
+categoria = rdm.choice(list(palavras.keys()))
+palavra = rdm.choice(palavras[categoria])
+
+descoberta = False
+
